97b9a2202780aacff653f643996cd07c2205fc01
apoorv <vappor.washmade@gmail.com>
2020-04-26 18:24:43 +0530

    set up for exam

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # apcs-2020
 This is the repo that contains my response to the AP CS 2020 exam.
+
+I am preparing this repo to write my responses to the 2020 AP CS Free Response Questions. 
---
+++ src/main/java/org/apoorv/ap/cs2020/Welcome.java
@@ -0,0 +1,10 @@
+package org.apoorv.ap.cs2020;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("This is my first AP CS Exam online. Time now: " + LocalDateTime.now().toString());
+    }
+}
