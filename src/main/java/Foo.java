6cadabf83c01613c3ff744e3b3ef9cad4568ff95
apoorv <vappor.washmade@gmail.com>
2020-05-13 07:58:38 +0530

    move to unnamed package

--- src/main/java/Foo.java
+++ src/main/java/Foo.java
@@ -1,9 +1,12 @@
-package org.apoorv.ap.cs2020;
+// We use default unnamed package
+/*
+AP ID: 1X91VY24
+Name: Apoorv Mhaswade
+*/
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-public class Welcome {
+public class Foo {
     public static void main(String[] args) {
         System.out.println("This is my first AP CS Exam online. Time now: " + LocalDateTime.now().toString());
     }
